bd6ee7d1ee38fd434027844d5abae836559b24a7
Aravinthishu <aravintharavinth7746@gmail.com>
2024-06-12 17:18:31 -0700

    Update code

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,49 @@
+import{
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImInstagram,
+  ImYoutube,
+}from 'react-icons/im'
+
 import React from 'react';
 
+
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='#' target='_blank'>
+          <ImFacebook />
+        </a>
+      </li>
+
+      <li>
+        <a href='#' target='_blank'>
+          <ImTwitter />
+        </a>
+      </li>
+
+      <li>
+        <a href='#' target='_blank'>
+          <ImPinterest />
+        </a>
+      </li>
+
+      <li>
+        <a href='#' target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+
+      <li>
+        <a href='#' target='_blank'>
+          <ImYoutube />
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
